f42878624547ce8adf1639ca0e5f6d438436ffd2
achan <44986059+ArthurChan0427@users.noreply.github.com>
2022-03-13 11:21:50 -0600

    created all static view layout and connection status menu bar (#5)
    * created all static view layout and connection status menu bar
    
    * Add reference to spinner

---
+++ Smartpad-macOS/Smartpad-macOS/Logic/ConnStatus.swift
@@ -0,0 +1,12 @@
+//
+//  ConnStatus.swift
+//  Smartpad-macOS
+//
+//  Created by Arthur Chan on 2022-03-13.
+//
+
+public enum ConnStatus {
+    case Unpaired
+    case PairedAndConnected
+    case PairedAndDisconnected
+}
