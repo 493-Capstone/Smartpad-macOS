4ef4787cf85b4b51318c4792fb5198461cd6f85d
huddy987 <35442144+huddy987@users.noreply.github.com>
2022-03-18 17:33:45 -0600

    Implement initial gesture recognition
    Recognize incoming gesture packet based on the packet's GestureType.
    
    Closes: SMAR-71, SMAR-58

---
+++ Smartpad-macOS/Smartpad-macOS/Logic/GestureRecognition/GestureGenerator.swift
@@ -0,0 +1,24 @@
+//
+//  GestureGenerator.swift
+//  Smartpad-macOS
+//
+//  Created by Hudson Shykowski on 2022-03-18.
+//
+
+import Foundation
+
+class GestureGenerator {
+    // Array of all classes that implement Gesture
+    static let gestures: [Gesture.Type] = [ZoomGesture.self]
+
+    static func getGesture(type: GestureType) -> Gesture.Type {
+        for gesture in gestures {
+            if (gesture.getGesture() == type) {
+                return gesture
+            }
+        }
+
+        // Unknown gesture type!
+        assert(false);
+    }
+}
---
+++ Smartpad-macOS/Smartpad-macOS/Logic/GestureType.swift
@@ -0,0 +1,15 @@
+//
+//  GestureType.swift
+//  Smartpad-iOS
+//
+//  Created by Hudson Shykowski on 2022-03-18.
+//
+
+// Documentation on various iOS gesture recognizers:
+// Pinch: https://developer.apple.com/documentation/uikit/touches_presses_and_gestures/handling_uikit_gestures/handling_pinch_gestures
+//
+//
+
+public enum GestureType: Codable {
+    case Pinch
+}
