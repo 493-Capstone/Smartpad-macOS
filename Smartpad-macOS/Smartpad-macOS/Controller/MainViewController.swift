c61959f210788e50a23b4abd9dc98a557cd5c50f
huddy987 <35442144+huddy987@users.noreply.github.com>
2022-03-28 17:38:25 -0600

    Name window and don't allow resizing

--- Smartpad-macOS/Smartpad-macOS/Controller/MainViewController.swift
+++ Smartpad-macOS/Smartpad-macOS/Controller/MainViewController.swift
@@ -25,11 +25,15 @@ class ViewController: NSViewController{
     }
     
     override func viewDidAppear() {
-        if (connData.getDeviceName() != ""){
-            print("get device name")
-            print(connData.getDeviceName())
+        self.view.window?.title = "Smartpad"
+
+        /* Don't allow resizing the window */
+        super.view.window?.styleMask.remove(.resizable)
+
+        /* If an identifier was already set, transition to the main view */
+        if (connData.getDeviceName() != "") {
+            print("Device name: ", connData.getDeviceName())
             if let controller = self.storyboard?.instantiateController(withIdentifier: "PairView") as? PairViewController {
-                print("transit")
                 self.view.window?.contentViewController = controller
             }
             
